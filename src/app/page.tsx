2fa1b267a180eb46f4ac425bce8255f23f9ca06e
aldcejam <aldcejamj923@gmail.com>
2023-05-31 10:47:51 -0300

    Etapa 4: Modificar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
 "use client"
-import { ShoppingCartDataStates } from '@/@core/ShoppingCartData/ShoppingCartData'
-import { useAppSelector } from '@/redux.config/hook'
-import { useState } from 'react'
-
-
+import { ShoppingCartDataStates, setID, setShoppingCartItems } from '@/@core/ShoppingCartData/ShoppingCartData'
+import { useAppDispatch, useAppSelector } from '@/redux.config/hook'
+ 
 export default function Home() {
 
   /* Usar dados do carrinho de compras */
   const id = useAppSelector(ShoppingCartDataStates).ID
   const shoppingCartItems = useAppSelector(ShoppingCartDataStates).shoppingCartItems
-  const [showData, setShowData] = useState(false)
-  const GetData = () => {
-    return `Id: ${id} - carrinho de compras: ${JSON.stringify(shoppingCartItems)}`
-  }
-  
+
+
+  /* Modificar dados do carrinho de compras */
+  const dispatch = useAppDispatch();
+  const modificarID = () => dispatch(setID("999"))
+  const modificarShoppingCartItems = () => dispatch(setShoppingCartItems({
+    carne: "2kg",
+    arroz: "1kg"
+  }))
 
 
   return (
     <div>
-      <div onClick={() => { GetData(), setShowData(true) }}>
+      <div>
         <p>
-          {showData ? GetData() : "Clique para vizualizar os dados"}
+          {`Id: ${id} - carrinho de compras: ${JSON.stringify(shoppingCartItems)}`}
         </p>
+      </div>
+      <div>
+        <button onClick={modificarID}>Modificar ID</button><br />
+        <button onClick={modificarShoppingCartItems}>Modificar shoppingCartItems</button>
       </div>
     </div>
   )
