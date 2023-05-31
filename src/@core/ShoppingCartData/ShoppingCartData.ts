b9618e760e3367c2fde27be0db591edc698ecdb1
aldcejam <aldcejamj923@gmail.com>
2023-05-31 10:27:40 -0300

    Etapa 3.2: implementando InitialStates e set(s)

--- src/@core/ShoppingCartData/ShoppingCartData.ts
+++ src/@core/ShoppingCartData/ShoppingCartData.ts
@@ -1,18 +1,30 @@
 import { RootState } from "@/redux.config/store";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IDProps, IShoppingCartDataProps, ShoppingCartItemsProps } from "./IShoppingCartData"
 
-const initialState = {
-
+const initialState: IShoppingCartDataProps = {
+    ID: "" as IDProps,
+    shoppingCartItems: {} as ShoppingCartItemsProps
 }
 
 export const ShoppingCartDataSlice = createSlice({
     name: "ShoppingCartData",
     initialState,
     reducers: {
+        setID: (state, action: PayloadAction<IDProps>) => {
+            state.ID = action.payload
+        },
+        setShoppingCartItems: (state, action: PayloadAction<ShoppingCartItemsProps>) => {
+            state.shoppingCartItems = action.payload
+        }
     }
 })
 
-export const { } = ShoppingCartDataSlice.actions
+export const {
+    setID,
+    setShoppingCartItems
+
+} = ShoppingCartDataSlice.actions
 
 export const ShoppingCartDataStates = (state: RootState) => state.ShoppingCartData
 
